f2cc5e69272e1ac12b79c0ff425fcd19e8af4767
Marius Cetateanu <mce@softkinetic.com>
2014-08-05 16:37:33 +0200

    Add preference summary.

--- app/src/main/java/com/example/mce/sunshine/SettingsFragment.java
+++ app/src/main/java/com/example/mce/sunshine/SettingsFragment.java
@@ -1,14 +1,39 @@
 package com.example.mce.sunshine;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    public static final String KEY_PREF_CITY = "sunshine_city";
+    public static final String KEY_PREF_UNIT = "sunshine_unit";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(KEY_PREF_CITY) || key.equals(KEY_PREF_UNIT)) {
+            findPreference(key).setSummary(sharedPreferences.getString(key, ""));
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public void onPause() {
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
     }
 }
